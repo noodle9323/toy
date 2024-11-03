3db5f863de90e459f71d258cb50c3260ab213996
noodle9323 <noodle9323@gmail.com>
2024-11-04 01:34:28 +0900

    settings

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TOY
+
+npx supabase gen types --lang=typescript --project-id "oelfldocmaetakljhoux" --schema public > database.types.ts
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,8 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: ["class"],
-    content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+	darkMode: ["class"],	
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
   	extend: {
   		colors: {
